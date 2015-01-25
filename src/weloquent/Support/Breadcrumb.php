c0d6e7dfafcce198fd6c95881d19f07ccee5fb73
Bruno Barros <brunodanca@gmail.com>
2015-01-25 10:53:40 -0200

    added custom post types abilities to Breadcrump

--- src/weloquent/Support/Breadcrumb.php
+++ src/weloquent/Support/Breadcrumb.php
@@ -33,23 +33,32 @@ class Breadcrumb
 	 * @var null
 	 */
 	protected $homeTitle = null;
+	/**
+	 * @var array
+	 */
+	protected $args = [
+		'taxonomies' => null,
+		'overload' => []
+	];
 
-	function __construct($wpPost = null)
+	function __construct($wpPost = null, array $args = [])
 	{
 		global $post;
 
 		$this->post = new PagePresenter((!$wpPost) ? $post : $wpPost);
+		$this->args = array_merge($this->args, $args);
 	}
 
 
 	/**
 	 * Quick way to get the breadcrumb
 	 *
+	 * @param array $args
 	 * @return mixed
 	 */
-	public static function make()
+	public static function make(array $args = [])
 	{
-		$bc = new static;
+		$bc = new static(null, $args);
 
 		return $bc->get();
 	}
@@ -72,13 +81,6 @@ class Breadcrumb
 				'url'   => get_option('home')
 			];
 
-			// if is custom post type
-			$postType = $this->post->postType;
-			if (!in_array($postType->name, $this->excludedPostTypes) && !is_category())
-			{
-				$b[] = ['title' => $postType->label, 'url' => $postType->permalink];
-			}
-
 			if (is_category())
 			{
 				if ($c = get_category(get_query_var('cat')))
@@ -87,15 +89,57 @@ class Breadcrumb
 				}
 
 			}
+			else if (is_post_type_archive())
+			{
+				$postType = $this->post->postType;
+				if (!in_array($postType->name, $this->excludedPostTypes))
+				{
+					$b[] = ['title' => $postType->label, 'url' => null];
+				}
+
+			}
+			else if (is_tax())
+			{
+				// if is custom post type
+				$postType = $this->post->postType;
+				if (!in_array($postType->name, $this->excludedPostTypes))
+				{
+					$b[] = ['title' => $postType->label, 'url' => $postType->permalink];
+				}
+
+				$tax = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+				$b[] = ['title' => $tax->name, 'url' => null];
+
+			}
 			else if (is_single())
 			{
-				if($categories = $this->post->categories)
+
+				// if is custom post type
+				$postType = $this->post->postType;
+				if (!in_array($postType->name, $this->excludedPostTypes))
 				{
-					foreach ($categories as $c)
+					$b[] = ['title' => $postType->label, 'url' => $postType->permalink];
+				}
+				else if (in_array($postType->name, $this->excludedPostTypes)
+					&& $custom = $this->getOverloaded($postType->name))
+				{
+					$b[] = ['title' => $custom->labels->name, 'url' => $custom->permalink];
+				}
+
+
+				if ($this->post->categories)
+				{
+					foreach ($this->post->categories as $c)
 					{
 						$b[] = ['title' => $c->name, 'url' => $c->permalink];
 					}
 				}
+
+				if ($tax = $this->getCurrentTaxonomy())
+				{
+					$b[] = ['title' => $tax->name, 'url' => get_term_link($tax)];
+				}
+
 				$b[] = ['title' => $this->post->title, 'url' => false];
 
 			}
@@ -124,7 +168,7 @@ class Breadcrumb
 			{
 				$b[] = ['title' => single_tag_title('', false), 'url' => false];
 			}
-			else if(is_author())
+			else if (is_author())
 			{
 				$b[] = ['title' => sprintf(__('Posts by %s'), get_the_author()), 'url' => false];
 			}
@@ -149,6 +193,46 @@ class Breadcrumb
 		}
 
 		return Collection::make($b);
+	}
+
+	/**
+	 * Find a custom taxonomy
+	 *
+	 * @return mixed|null
+	 */
+	protected function getCurrentTaxonomy()
+	{
+		if (!isset($this->args['taxonomies']) || empty($this->args['taxonomies']))
+		{
+			return null;
+		}
+
+		foreach ($this->args['taxonomies'] as $tax)
+		{
+			if ($tax = get_term_by('slug', get_query_var($tax), $tax))
+			{
+				return $tax;
+				break;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * return the fake post type
+	 *
+	 * @param $postType
+	 * @return null
+	 */
+	protected function getOverloaded($postType)
+	{
+		if(!isset($this->args['overload'][$postType]))
+		{
+			return null;
+		}
+
+		return $this->args['overload'][$postType];
 	}
 
 }
