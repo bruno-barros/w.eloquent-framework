a9d6c3fe83709d6004656bad809bf1a0f8722a7b
Bruno Barros <brunodanca@gmail.com>
2015-06-18 21:52:35 -0300

    comment brain libs

--- src/weloquent/Plugins/BrainPlugins/brain-plugins.php
+++ src/weloquent/Plugins/BrainPlugins/brain-plugins.php
@@ -19,21 +19,25 @@ $vendor = str_replace(DS.'src', '', SRC_PATH);
 add_action('brain_init', function ($brain) use ($vendor)
 {
 
+    // request
 	if (file_exists($vendor . '/vendor/brain/amygdala/src/BrainModule.php'))
 	{
 		$brain->addModule(new Brain\Amygdala\BrainModule);
 	}
 
+    // hooks
 	if (file_exists($vendor . '/vendor/brain/striatum/src/BrainModule.php'))
 	{
 		$brain->addModule(new Brain\Striatum\BrainModule);
 	}
 
+    // route
 	if (file_exists($vendor . '/vendor/brain/cortex/src/BrainModule.php'))
 	{
 		$brain->addModule(new Brain\Cortex\BrainModule);
 	}
 
+    // assets
 	if (file_exists($vendor . '/vendor/brain/occipital/src/BrainModule.php'))
 	{
 		$brain->addModule(new Brain\Occipital\BrainModule);
