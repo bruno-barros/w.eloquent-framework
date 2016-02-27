2a140433f83fe8a62979a8b65624f3ad3877123d
Bruno Barros <brunodanca@gmail.com>
2016-02-26 21:57:00 -0300

    remove unused brain modules

--- src/weloquent/Plugins/BrainPlugins/brain-plugins.php
+++ src/weloquent/Plugins/BrainPlugins/brain-plugins.php
@@ -18,31 +18,10 @@ $vendor = str_replace(DS.'src', '', SRC_PATH);
 
 add_action('brain_init', function ($brain) use ($vendor)
 {
-
-    // request
-	if (file_exists($vendor . '/vendor/brain/amygdala/src/BrainModule.php'))
-	{
-		$brain->addModule(new Brain\Amygdala\BrainModule);
-	}
-
-    // hooks
-	if (file_exists($vendor . '/vendor/brain/striatum/src/BrainModule.php'))
-	{
-		$brain->addModule(new Brain\Striatum\BrainModule);
-	}
-
-    // route
 	if (file_exists($vendor . '/vendor/brain/cortex/src/BrainModule.php'))
 	{
 		$brain->addModule(new Brain\Cortex\BrainModule);
 	}
-
-    // assets
-	if (file_exists($vendor . '/vendor/brain/occipital/src/BrainModule.php'))
-	{
-		$brain->addModule(new Brain\Occipital\BrainModule);
-	}
-
 });
 
 add_action('setup_theme', function ()
